7ece0e7a1c3f4da4ab68d9020f0b8914178f4353
RazuvaevDD <Razuvaev_DD@mail.ru>
2022-11-18 22:44:53 +0300

    Inital commit

---
+++ core/src/com/razuvaevdd/Main.java
@@ -0,0 +1,22 @@
+package com.razuvaevdd;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class Main extends ApplicationAdapter {
+	
+	@Override
+	public void create () {
+	}
+
+	@Override
+	public void render () {
+		ScreenUtils.clear(.5f, .5f, .5f, 1);
+	}
+	
+	@Override
+	public void dispose () {
+	}
+}
